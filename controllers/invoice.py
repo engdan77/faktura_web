b53f08ccd1a9550eb80025ad4a82a3abf7cc2856
Daniel Engvall <daniel@engvalls.eu>
2018-12-03 09:02:43 +0100

    Adding button for deletions

--- controllers/invoice.py
+++ controllers/invoice.py
@@ -1,7 +1,7 @@
 def create():
     form = SQLFORM.factory(db.invoice, formstyle='table3cols', submit_button='Lägg till')
     if form.accepts(request, session):
-        result_id = db.invoice.insert(**db.invoice._filter_fields(form.vars))
+        db.invoice.insert(**db.invoice._filter_fields(form.vars))
         response.flash = 'Faktura skapad'
     grid_invoice = SQLFORM.grid(db.invoice,
                                 editable=True,
@@ -9,15 +9,46 @@ def create():
                                 searchable=False,
                                 create=False,
                                 csv=False,
-                                links=[lambda row: A('Produkter',_href=URL("default","show",args=[row.id]))]
+                                links=[lambda row: A('Produkter',_href=URL("invoice", "service_to_invoice", vars={'invoice_id': row.id}))]
                                 )
     [button.__setitem__(0, 'Titta') for button in grid_invoice.elements('span[title=%s]' % T('View'))]
     [button.__setitem__(0, 'Ta bort') for button in grid_invoice.elements('span[title=%s]' % T('Delete'))]
     [button.__setitem__(0, 'Tillbaka') for button in grid_invoice.elements('span[title=%s]' % T('Back'))]
     return dict(form=form, grid_invoice=grid_invoice)
 
-def map_service_to_invoice():
-    form = SQLFORM.factory(db.invoice_service_mapping, formstyle='table3cols', submit_button='Lägg till').process()
-    if form.accepted:
-        response.flash = 'Produkt tillagd'
-    return dict(form=form)
+def service_to_invoice():
+    invoice_id = request.vars['invoice_id']
+    db.invoice_service_mapping.invoice_id.default = invoice_id
+    db.invoice_service_mapping.invoice_id.writable = False
+    form = SQLFORM.factory(db.invoice_service_mapping,
+                           formstyle='table3cols',
+                           submit_button='Lägg till',
+                            )
+
+    if form.accepts(request, session):
+        db.invoice_service_mapping.insert(**db.invoice_service_mapping._filter_fields(form.vars))
+        response.flash = 'Lagt till produkt'
+
+    db.service.id.writable = False
+    db.service.id.readable = False
+    db.invoice_service_mapping.id.writable = False
+    db.invoice_service_mapping.id.readable = False
+    db.invoice_service_mapping.invoice_id.writable = False
+    db.invoice_service_mapping.invoice_id.readable = False
+    db.invoice_service_mapping.service_id.writable = False
+    db.invoice_service_mapping.service_id.readable = False
+    grid_services = SQLFORM.grid(db.invoice_service_mapping,
+                                editable=True,
+                                deletable=False,
+                                searchable=False,
+                                create=False,
+                                csv=False,
+                                left=(db.service.on(db.invoice_service_mapping.service_id == db.service.id)),
+                                 selectable=[('Ta bort',
+                                              lambda ids: redirect(URL('default', 'mapping_multiple',
+                                                                       vars=dict(id=ids))))]
+                                )
+    [button.__setitem__(0, 'Titta') for button in grid_services.elements('span[title=%s]' % T('View'))]
+    [button.__setitem__(0, 'Ta bort') for button in grid_services.elements('span[title=%s]' % T('Delete'))]
+    [button.__setitem__(0, 'Tillbaka') for button in grid_services.elements('span[title=%s]' % T('Back'))]
+    return dict(form=form, grid_services=grid_services)
