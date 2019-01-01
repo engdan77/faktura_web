11da207dde5ebd8d7b7f9b4a6b712bc5a9104147
Daniel Engvall <daniel@engvalls.eu>
2019-01-01 12:31:11 +0100

    Update invoice_writer

--- modules/invoice_writer.py
+++ modules/invoice_writer.py
@@ -69,5 +69,5 @@ def create_pdf(invoice_file, input_dict):
 
     # Optional
     doc.set_bottom_tip(
-        f"Email: {d['company_email']}<br />Kontonummer: {d['company_account']}<br />Vid frågor kontakt på {d['company_phone_number']}.")
+        f"Email: {d['company_email']}<br />Kontonummer: {d['company_account']}<br />Godkänd för F-skatt<br />Vid frågor kontakt på {d['company_phone_number']}.")
     doc.finish()
